78e7074aca8ab8ee196020ba803be6d4bedd565e
Remy D. Farley <one-d-wide@protonmail.com>
2025-08-17 18:46:42 +0000

    Fix truncated response

--- src/lib.rs
+++ src/lib.rs
@@ -29,15 +29,14 @@ compile_error!(
     "\"use_tokio\" and \"use_futures\" features can't be enabled at the same time. Consider choosing only one"
 );
 
-// std
 #[cfg(feature = "use_std")]
-use std::io::{BufRead as AsyncBufRead, BufReader, Read as AsyncRead, Write as AsyncWrite};
-// tokio
+use std::io::{Read as AsyncRead, Write as AsyncWrite};
+
 #[cfg(feature = "use_tokio")]
-use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
-// futures
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+
 #[cfg(feature = "use_futures")]
-use futures::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
+use futures::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use {
     maybe_async::maybe_async,
@@ -58,7 +57,8 @@ pub enum RouterVersion {
 
 #[derive(Debug)]
 pub struct Endpoint<S> {
-    socket: BufReader<S>,
+    scratch: Vec<u8>,
+    socket: S,
     router_version: RouterVersion,
 }
 
@@ -67,7 +67,8 @@ impl<S: AsyncWrite + AsyncRead + Unpin> Endpoint<S> {
     pub async fn attach(socket: S) -> Self {
         // Assume router is of last known version
         let mut endpoint = Self {
-            socket: BufReader::new(socket),
+            scratch: Vec::new(),
+            socket,
             router_version: RouterVersion::v0_5_0__,
         };
 
@@ -99,7 +100,8 @@ impl<S: AsyncWrite + AsyncRead + Unpin> Endpoint<S> {
 
     pub fn attach_version(socket: S, router_version: RouterVersion) -> Self {
         Self {
-            socket: BufReader::new(socket),
+            scratch: Vec::new(),
+            socket,
             router_version,
         }
     }
@@ -109,15 +111,15 @@ impl<S: AsyncWrite + AsyncRead + Unpin> Endpoint<S> {
     }
 
     pub fn into_inner(self) -> S {
-        self.socket.into_inner()
+        self.socket
     }
 
     pub fn get_ref(&self) -> &S {
-        self.socket.get_ref()
+        &self.socket
     }
 
     pub fn get_mut(&mut self) -> &mut S {
-        self.socket.get_mut()
+        &mut self.socket
     }
 
     #[maybe_async]
@@ -137,67 +139,204 @@ impl<S: AsyncWrite + AsyncRead + Unpin> Endpoint<S> {
             keepalive: true,
         };
         self.socket
-            .get_mut()
             .write_all(serde_json::to_vec(&request)?.as_slice())
             .await?;
 
-        let mut last_len = 0usize;
-        loop {
-            let frame = self.socket.fill_buf().await?;
-            // Handle EOF
-            if last_len == frame.len() {
-                return Err(Error::new(
-                    ErrorKind::UnexpectedEof,
-                    "Socket closed while awaiting data",
-                ));
-            }
-            last_len = frame.len();
+        let buf = protocol::read_response(&mut self.socket, &mut self.scratch).await?;
 
-            // Deserialize stream
-            let mut stream =
-                serde_json::Deserializer::from_slice(frame).into_iter::<protocol::Response<T>>();
-            if let Some(result) = stream.next() {
-                let parsed = result.map_err(|err| {
-                    Error::new(
-                        ErrorKind::InvalidData,
-                        format!(
-                            "While parsing endpoint response for request '{}': {err}",
-                            request.request
-                        ),
-                    )
-                })?;
-                let consumed = stream.byte_offset() + 1;
-                #[cfg(feature = "use_futures")]
-                self.socket.consume_unpin(consumed);
-                #[cfg(not(feature = "use_futures"))]
-                self.socket.consume(consumed);
-                return Ok(match (parsed.status.as_str(), parsed.response) {
-                    ("success", Some(response)) => Ok(response),
-                    _ => Err(parsed.error.unwrap_or("Unknown".to_string())),
-                });
-            }
-        }
+        let response: protocol::Response<T> = serde_json::from_slice(buf).map_err(|err| {
+            Error::new(
+                ErrorKind::InvalidData,
+                format!(
+                    "While parsing endpoint response for request {:?}: {err}",
+                    request.request
+                ),
+            )
+        })?;
+        return Ok(match (response.status.as_str(), response.response) {
+            ("success", Some(response)) => Ok(response),
+            _ => Err(response.error.unwrap_or_else(|| "Unknown".to_string())),
+        });
     }
 }
 
 mod protocol {
     use super::*;
+
     #[derive(Serialize)]
     pub struct Request<'a> {
         pub request: &'a str,
         pub keepalive: bool,
         pub arguments: HashMap<String, serde_json::Value>,
     }
+
     #[derive(Deserialize)]
     pub struct Response<T> {
         pub status: String,
         pub error: Option<String>,
         pub response: Option<T>,
     }
+
+    /// Currently there's no well known json deserializer supporting async,
+    /// so for the time being response separation is done using heuristics.
+    ///
+    /// This function assumes:
+    ///   - response is strictly formatted (pretty or compact format).
+    ///   - `reader` doesn't yield anything besides a single json object.
+    #[maybe_async]
+    pub async fn read_response<'a, R: AsyncRead + Unpin>(
+        reader: &mut R,
+        scratch: &'a mut Vec<u8>,
+    ) -> io::Result<&'a [u8]> {
+        if scratch.is_empty() {
+            scratch.extend(std::iter::repeat_n(0, 8192));
+        }
+        let mut len = 0;
+        loop {
+            if len == scratch.len() {
+                // Double the scratch buffer capacity
+                scratch.extend(std::iter::repeat_n(0, len));
+            }
+            let cap = scratch.len();
+            let read = reader.read(&mut scratch[len..cap]).await?;
+            if read == 0 {
+                // EOF
+                break;
+            }
+            len += read;
+            if len <= 2 {
+                continue;
+            }
+            let buf = &scratch[0..len];
+            if buf.starts_with(b"{\n") {
+                // Pretty
+                if buf.ends_with(b"\n}\n") {
+                    break;
+                }
+            } else {
+                // Compact
+                if buf.ends_with(b"}\n") {
+                    break;
+                }
+            }
+        }
+        Ok(&scratch[0..len])
+    }
 }
 
 #[cfg(test)]
+#[cfg(feature = "use_std")]
 mod tests {
+    use super::*;
+
+    use std::io::{Cursor, Read, Write};
+
+    macro_rules! mock_reader {
+        ($($read_count:expr => $slice:expr $(,)?)*) => {{
+            struct MockReader {
+                read_counter: u32,
+            }
+            impl Write for MockReader {
+                fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+                    Ok(buf.len())
+                }
+                fn flush(&mut self) -> io::Result<()> {
+                    Ok(())
+                }
+            }
+            impl Read for MockReader {
+                fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+                    self.read_counter += 1;
+                    match self.read_counter {
+                        $($read_count => {
+                            return Cursor::new(buf).write($slice);
+                        }),*
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            impl Drop for MockReader {
+                fn drop(&mut self) {
+                    let read_expected = 0 $(.max($read_count))*;
+                    if self.read_counter != read_expected {
+                        panic!("Mocked socket seen {} reads, while expected {}", self.read_counter, read_expected)
+                    }
+                }
+            }
+            MockReader { read_counter: 0 }
+        }};
+    }
+
+    #[test]
+    fn simple() {
+        let sock = mock_reader!(
+            1 => &{
+                let json = serde_json::json!({
+                    "status": "success",
+                    "request": {
+                        "request": "test",
+                        "arguments": {},
+                    },
+                    "response": {
+                        "mock": 42,
+                    }
+                });
+                let mut vec = serde_json::to_vec_pretty(&json).unwrap();
+                vec.push(b'\n');
+                vec
+            }
+        );
+        let mut e = Endpoint::attach_version(sock, RouterVersion::v0_5_0__);
+        #[derive(Debug, Deserialize, PartialEq, Eq)]
+        struct MockResult {
+            mock: u32,
+        }
+        let res: MockResult = Endpoint::request(&mut e, "test").unwrap().unwrap();
+        assert_eq!(res, MockResult { mock: 42 });
+    }
+
+    #[test]
+    fn read_response() {
+        use super::protocol::read_response;
+        let mut scratch = Vec::new();
+        assert_eq!(
+            read_response(&mut mock_reader!(1 => b"{ ... }", 2 => b""), &mut scratch).unwrap(),
+            b"{ ... }"
+        );
+        assert_eq!(
+            read_response(&mut mock_reader!(1 => b"{ ... }\n"), &mut scratch).unwrap(),
+            b"{ ... }\n"
+        );
+        assert_eq!(
+            read_response(&mut mock_reader!(1 => b"{\n ... \n}\n"), &mut scratch).unwrap(),
+            b"{\n ... \n}\n"
+        );
+        assert_eq!(
+            read_response(
+                &mut mock_reader!(
+                    1 => b"{",
+                    2 => b" ... ",
+                    3 => b"}\n",
+                ),
+                &mut scratch
+            )
+            .unwrap(),
+            b"{ ... }\n"
+        );
+
+        let line: String = std::iter::repeat_n('a', 100).collect();
+        let lines: String = std::iter::repeat_n(format!("\"{line}\",\n"), 1000).collect();
+        let long = format!("{{\n{lines}\n}}\n");
+        assert!(long.len() > 8192 << 2);
+        assert_eq!(
+            read_response(&mut Cursor::new(long.as_bytes()), &mut scratch).unwrap(),
+            long.as_bytes(),
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests_live {
     use super::*;
     const SOCKET_PATH: &str = env!("YGGDRASIL_SOCKET");
 
